374dd6d984f7b5f8c9ef683f71950c23533fd8ed
fbeltrao <fbeltrao@windowslive.com>
2018-09-03 12:24:30 +0200

    Add build id to FrontEnd Web

---
+++ src/FrontEnd.Web/ProjectProperties.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Web
+{
+    public static class ProjectProperties
+    {
+        /// <summary>
+        /// Build identifier
+        /// </summary>
+        public static string BuildId { get; set; }
+
+        static ProjectProperties()
+        {
+            BuildId = "#{Build.BuildId}#";
+            if ("#{Build.BuildId}#".Equals(BuildId))
+                BuildId = "0";
+        }
+    }
+}
